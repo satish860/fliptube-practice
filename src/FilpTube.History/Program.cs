92a5041738e6ebe0b27fe7c31db1c8859d8c6c63
Satish Venkatakrishnan <satish860@gmail.com>
2023-01-19 10:02:44 +0530

    Added the Worker service to the solution

---
+++ src/FilpTube.History/Program.cs
@@ -0,0 +1,17 @@
+namespace FilpTube.History
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<Worker>();
+                })
+                .Build();
+
+            host.Run();
+        }
+    }
+}
